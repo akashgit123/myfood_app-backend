40fa5affb1569e0938b21c4a42c2bcd380c5c2a8
Akash <akashshettigar709@gmail.com>
2023-12-04 11:17:00 +0530

    cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv').config();
+const cors = require('cors');
 const connectToDb = require('./db/foodApp');
 
 const port = process.env.PORT;
@@ -8,6 +9,7 @@ const port = process.env.PORT;
 connectToDb();
 
 app.use(express.json());
+app.use(cors())
 
 // routes
 app.use('/api/user',require('./routes/v1/user'));
